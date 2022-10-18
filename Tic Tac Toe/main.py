c5766e1f7b3bc671d07cc51174064a60ee1e0268
Dhyey Vyas <48405523+Dhyey17@users.noreply.github.com>
2022-10-18 22:05:37 +0530

    Add files via upload

---
+++ Tic Tac Toe/main.py
@@ -0,0 +1,25 @@
+import player
+from Game import TicTacToe, play
+
+print(f"1. Play Against Computer \n"
+      f"2. Play Against Human")
+choice = int(input("Enter Your Choice:"))
+
+
+class GameError(Exception):
+    pass
+
+
+if choice == 1:
+    playerX = player.HumanPlayer("X")
+    playerO = player.ComputerPlayer("O")
+    t = TicTacToe()
+    play(t, playerX, playerO)
+
+elif choice == 2:
+    playerX = player.HumanPlayer("X")
+    playerO = player.HumanPlayer("O")
+    t = TicTacToe()
+    play(t, playerX, playerO)
+else:
+    raise GameError("You did not entered a valid choice")
